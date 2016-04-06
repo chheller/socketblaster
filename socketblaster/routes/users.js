a019e0e5840ea687880725837ff069338a501881
iamsam <samuel.louis.milligan@gmail.com>
2016-04-05 23:22:27 -0500

    Login fixes

--- socketblaster/routes/users.js
+++ socketblaster/routes/users.js
@@ -12,6 +12,25 @@ router.get('/userlist', function(req, res) {
     });
 });
 
+router.post('/login/:email/:password', function(req, res) {
+  var db = req.db;
+  var collection = db.get('userlist');
+  var emailn = req.params.email;
+  var password = req.params.password;
+  collection.find({ 'email' : emailn }, { 'email' : { $exists : true }}, function(e,docs){
+    if(e === null) {
+      if(docs[0] != null) {
+        if(password == docs[0].password) {
+          console.log("password match");
+          res.send( { msg: "true"} );
+        }
+        else {res.send( { msg: "wrong credentials"} )}
+      }
+      else {res.send( { msg: "wrong credentials"} )}
+    }
+  });
+});
+
 /*
  * POST to adduser.
  */
