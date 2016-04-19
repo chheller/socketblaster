1ce3fe9155465676558ffcb46dc14ad12c4604aa
iamsam <samuel.louis.milligan@gmail.com>
2016-04-19 01:30:08 -0500

    Stored Messages in the Database

--- socketblaster/app.js
+++ socketblaster/app.js
@@ -14,6 +14,7 @@ var db = monk('localhost:27017/nodetest2');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var listings = require('./routes/listings');
+var conversations = require('./routes/conversations');
 
 var app = express();
 app.io = require('socket.io')();
@@ -41,6 +42,7 @@ app.use(function (req, res, next) {
 app.use('/', routes(app.io));
 app.use('/users', users);
 app.use('/listings', listings);
+app.use('/conversations', conversations);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
